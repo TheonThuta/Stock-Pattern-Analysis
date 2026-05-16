566ec03ab52d2d19b9f2009c1259eb58efcfdcf4
TheonThuta <thutatheon@gmail.com>
2026-05-16 16:56:46 +0900

    Initial predictions calculated by short and long term moving averages

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
 from fetcher import get_stock_data
+from predictor import get_prediction
 
 app = Flask(__name__)
 
@@ -10,6 +11,11 @@ def home():
 @app.route('/api/stock/<ticker>/<period>')
 def stock(ticker, period):
     return get_stock_data(ticker, '1d', period).to_json(orient='records', date_format='iso')
+
+@app.route('/api/predict/<ticker>')
+def predict(ticker):
+    return jsonify(get_prediction(ticker))
+
 
 if __name__ == '__main__':
     app.run()
